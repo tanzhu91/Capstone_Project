b8cabfe79fb97875c56cf4e27bf35bae3fb8c48d
tanzhu91 <tanzhu.dereli91@gmail.com>
2025-01-14 00:29:27 +0100

    Update app.py

--- app.py
+++ app.py
@@ -208,13 +208,13 @@ st.markdown("""
 
 
 
-app = st.navigation([
-    st.Page("pages/intro.py", title="Introduction"),
-    st.Page("pages/financial.py", title="Numbers and Revenue"),
-    st.Page("pages/platform.py", title ="Platform, Ratings and top games"),
-    st.Page("pages/genres.py", title ="Genres, Categories and Playtime"),
-    st.Page("pages/conclusion.py", title="Conclusion"),
-    st.Page("pages/references.py", title=" References")
-])
-app.run()
+#app = st.navigation([
+    #st.Page("pages/intro.py", title="Introduction"),
+    #st.Page("pages/financial.py", title="Numbers and Revenue"),
+    #st.Page("pages/platform.py", title ="Platform, Ratings and top games"),
+    #st.Page("pages/genres.py", title ="Genres, Categories and Playtime"),
+    #st.Page("pages/conclusion.py", title="Conclusion"),
+    #st.Page("pages/references.py", title=" References")
+#])
+#app.run()
 
