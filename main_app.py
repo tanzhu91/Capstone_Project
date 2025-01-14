1cd04d8e2df2bf0ead633e9e372cc274f3acac1f
tanzhu91 <tanzhu.dereli91@gmail.com>
2025-01-14 17:05:02 +0100

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -4,6 +4,7 @@ import plotly.graph_objects as go
 import warnings
 warnings.filterwarnings("ignore")
 
+st.set_page_config(page_title="My Wide App", layout="wide")
 
 custom_template = {
     "layout": {
