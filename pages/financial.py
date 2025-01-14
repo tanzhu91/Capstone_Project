8fb7664144461593da2afb82ab2ea20c77d31f1f
tanzhu91 <tanzhu.dereli91@gmail.com>
2025-01-14 18:50:30 +0100

    Update financial.py

--- pages/financial.py
+++ pages/financial.py
@@ -19,7 +19,7 @@ if response.status_code == 200:
 else:
     print(f"Failed to download the file. Status code: {response.status_code}")
     
-data = pd.read_csv("./games_prepped.csv", low_memory=False)
+data = pd.read_csv("./games_prepped.csv", low_memory=False,chunksize=50)
 
 st.title("Number of games and Revenue analysis")
 
